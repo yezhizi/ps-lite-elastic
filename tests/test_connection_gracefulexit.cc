679c5ec92a6dfdce7ef947e28bb9619d4a150b03
DeEMO <yzzxrx@foxmail.com>
2024-01-09 20:35:36 +0800

    :bug: Fix gitignore mistakes

---
+++ tests/test_connection_gracefulexit.cc
@@ -0,0 +1,26 @@
+#include "ps/ps.h"
+#include <random>
+
+int generate_random_number(int min, int max) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distrib(min, max);
+
+  return distrib(gen);
+}
+
+int main(int argc, char *argv[]) {
+  ps::Start(0, "doNotBarrier");
+  bool is_scheduler = ps::IsScheduler();
+  if(!is_scheduler){
+    int sleep_time = generate_random_number(10, 20);
+    std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
+  }else{
+    while(true){
+      std::this_thread::sleep_for(std::chrono::seconds(1000));
+    }
+  }
+  
+  ps::Finalize(0, false);
+  return 0;
+}
