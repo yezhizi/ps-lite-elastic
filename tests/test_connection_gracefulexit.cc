7061b73d2ecd517620887461c45bfa5eb34a08b6
fengwenjiao <1215696582@qq.com>
2024-04-29 10:20:36 +0800

    some try

--- tests/test_connection_gracefulexit.cc
+++ tests/test_connection_gracefulexit.cc
@@ -1,6 +1,6 @@
 #include "ps/ps.h"
 #include <random>
-#include <iostream> 
+#include <iostream>
 
 int generate_random_number(int min, int max) {
   std::random_device rd;
@@ -12,20 +12,21 @@ int generate_random_number(int min, int max) {
 
 int main(int argc, char *argv[]) {
   ps::StartAsync(0);
-  if(!ps::Postoffice::Get()->is_scale()){
-    ps::Postoffice::Get()->Barrier(0, ps::kWorkerGroup + ps::kServerGroup + ps::kScheduler);
+  if (!ps::Postoffice::Get()->is_scale()) {
+    ps::Postoffice::Get()->Barrier(
+        0, ps::kWorkerGroup + ps::kServerGroup + ps::kScheduler);
   }
-  std::cout<< "Start !!" << std::endl;
-  bool is_scheduler = ps::IsScheduler();
-  if(!is_scheduler){
+  std::cout << "Start !!" << std::endl;
+  bool is_scheduler = ps::IsScheduler();  // 冯文佼分支
+  if (!is_scheduler) {
     int sleep_time = generate_random_number(100000, 200000);
     std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
-  }else{
-    while(true){
+  } else {
+    while (true) {
       std::this_thread::sleep_for(std::chrono::seconds(1000));
     }
   }
-  
+
   ps::Finalize(0, false);
   return 0;
 }
